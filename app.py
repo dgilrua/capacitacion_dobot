d96019943fedad88344302345ec4b7539baab2b6
David Gil Rua <dgilr@unal.edu.co>
2024-05-28 07:08:58 -0500

    modbus

--- app.py
+++ app.py
@@ -17,14 +17,10 @@ class ArrowKeyApp:
         
         self.canvas = tk.Canvas(root, width=300, height=300)
         self.canvas.pack()
-
-        self.response = "asdasd"
         
         self.label = tk.Label(root, text=self.update_label_text(), font=("Helvetica", 16))
         self.label.pack()
         
-        self.label2 = tk.Label(root, text=self.response, font=("Helvetica", 16))
-        self.label2.pack()
         
         self.create_buttons()
 
@@ -34,46 +30,66 @@ class ArrowKeyApp:
 
         button_size = {"width": 5, "height": 2, "font": ("Helvetica", 24)}
 
-        up_button = tk.Button(button_frame, text="↑", command=self.increase_y, **button_size)
-        up_button.grid(row=0, column=1)
+        tk.Label(button_frame, text="X").grid(row=0, column=0)
+        up_button_x = tk.Button(button_frame, text="↑", command=self.increase_x, **button_size)
+        up_button_x.grid(row=1, column=0)
 
-        left_button = tk.Button(button_frame, text="←", command=self.decrease_x, **button_size)
-        left_button.grid(row=1, column=0)
-
-        right_button = tk.Button(button_frame, text="→", command=self.increase_x, **button_size)
-        right_button.grid(row=1, column=2)
-
-        down_button = tk.Button(button_frame, text="↓", command=self.decrease_y, **button_size)
-        down_button.grid(row=2, column=1)
+        down_button_x = tk.Button(button_frame, text="↓", command=self.decrease_x, **button_size)
+        down_button_x.grid(row=2, column=0)
         
+        tk.Label(button_frame, text="Y").grid(row=0, column=1)
+        up_button_y = tk.Button(button_frame, text="↑", command=self.increase_y, **button_size)
+        up_button_y.grid(row=1, column=1)
+
+        down_button_y = tk.Button(button_frame, text="↓", command=self.decrease_y, **button_size)
+        down_button_y.grid(row=2, column=1)
+        
+        tk.Label(button_frame, text="Z").grid(row=0, column=2)
+        up_button_z = tk.Button(button_frame, text="↑", command=self.increase_z, **button_size)
+        up_button_z.grid(row=1, column=2)
+
+        down_button_x = tk.Button(button_frame, text="↓", command=self.decrease_z, **button_size)
+        down_button_x.grid(row=2, column=2)
+
         open_button = tk.Button(button_frame, text="Open", command=self.open, **button_size)
         open_button.grid(row=3, column=0)
-        
-        send_button = tk.Button(button_frame, text="Send", command=self.send, **button_size)
-        send_button.grid(row=3, column=1)
         
         close_button = tk.Button(button_frame, text="Close", command=self.close, **button_size)
         close_button.grid(row=3, column=2)
 
 
     def update_label_text(self):
-        return f"X: {self.x}, Y: {self.y}"
+        return f"X: {self.x}, Y: {self.y}, Z: {self.z}"
 
     def increase_x(self):
-        self.x += 1
+        self.x += 10
         self.update_display()
+        self.send()
 
     def decrease_x(self):
-        self.x -= 1
+        self.x -= 10
         self.update_display()
+        self.send()
+
+    def increase_z(self):
+        self.z += 10
+        self.update_display()
+        self.send()
+
+    def decrease_z(self):
+        self.z -= 10
+        self.update_display()
+        self.send()
 
     def increase_y(self):
-        self.y += 1
+        self.y += 10
         self.update_display()
+        self.send()
 
     def decrease_y(self):
-        self.y -= 1
+        self.y -= 10
         self.update_display()
+        self.send()
 
     def update_display(self):
         self.label.config(text=self.update_label_text())
@@ -85,7 +101,7 @@ class ArrowKeyApp:
         self.s.send("close".encode())
         
     def send(self):
-        self.s.send("go".encode())
+        self.s.send(f"{self.x},{self.y},{self.z},{0}".encode())
         
 if __name__ == "__main__":
     root = tk.Tk()
